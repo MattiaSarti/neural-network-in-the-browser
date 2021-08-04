086bf0ca2e8187a8e9bc8213f7cf07d350bd0144
MattiaSarti <nmattia1@gmail.com>
2021-08-04 17:25:22 +0200

    🍃 ADD - initial POC 🍂

---
+++ sentiment_update.js
@@ -0,0 +1,57 @@
+let background;
+let button;
+let happiness_probability;
+let image;
+let input;
+let model;
+let next_color;
+let style;
+
+
+function getSentiment() {
+  const input_text = input.value;
+  const prediction = model.predict(input_text);
+
+  return prediction.score;
+}
+
+
+function setup() {
+  model = ml5.sentiment('movieReviews', function(){console.log("model loaded");});
+
+  style = document.body.style;
+  button = document.getElementById("button");
+  input = document.getElementById("input");
+  image = document.getElementById("image");
+
+  button.addEventListener('click', updateImageBasedOnSentiment);
+}
+
+
+function updateImageBasedOnSentiment() {
+  happiness_probability = getSentiment();
+
+  // next_image_name = ((happiness_probability > 0.5) ? "happiness.png" : "sadness.png");
+  switch (true) {
+
+    case (happiness_probability > 0.67):
+      next_image_name = "happiness.png";
+      next_color = "rgb(0, 162, 255)";
+      break;
+
+    case (happiness_probability > 0.33):
+      next_image_name = "neutrality.png";
+      next_color = "rgb(30, 112, 81)";
+      break;
+
+    default:
+      next_image_name = "sadness.png";
+      next_color = "rgb(84, 115, 151)";
+  }
+
+  image.src = next_image_name;
+  style.backgroundColor = next_color;
+}
+
+
+setup();
